79d124339f5db048da2b7aafaea1818c56c26a6c
Juan C. Gonzalez <juanc.gonzalez8@gmail.com>
2019-02-25 00:59:21 -0500

    Added a test

--- MyWorkout.Tests/BusinessLogic/WorkoutTest.cs
+++ MyWorkout.Tests/BusinessLogic/WorkoutTest.cs
@@ -7,19 +7,32 @@ namespace MyWorkout.BusinessLogic.Tests
     [TestClass]
     public class WorkoutTest
     {
-        //Confirm that an excersice is added to a workout.
-        [TestMethod]
-        public void TestAddExerciseToWorkout()
+        Workout workout = new Workout();
+        Set set = new Set();
+        public WorkoutTest()
         {
-            Workout workout = new Workout();
-            Set set = new Set();            
             set.Excercise.Name = "deadlift";
             set.Repetitions = 5;
             set.Weight = 255;
             set.StartTime = DateTime.Now;
-            set.FinishTime = DateTime.Now;
+            set.FinishTime = set.StartTime.AddMinutes(1);
+        }
+        //Confirm that an excercise is added to a 
+        //workout in the logic.
+        [TestMethod]
+        public void TestAddExerciseToWorkout()
+        {
             workout.Sets.Add(set);
             Assert.AreEqual("deadlift", workout.Sets[0].Excercise.Name);
+        }
+
+        //TODO Edit a set
+        [TestMethod]
+        public void TestEditSet()
+        {
+            workout.Sets.Add(set);
+            workout.Sets[0].Excercise.Name = "Squats";
+            Assert.AreEqual("Squats", workout.Sets[0].Excercise.Name);
         }
 
         //Get the total weight lifted (volume?) of the workout
