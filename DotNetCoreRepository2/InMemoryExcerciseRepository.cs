89db72fa3c0a7b76ad9f2528a5c3245774644cdd
Juan C. Gonzalez <juanc.gonzalez8@gmail.com>
2019-10-14 22:46:21 -0400

    updated to .net core 3.0.

---
+++ DotNetCoreRepository2/InMemoryExcerciseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Core;
+using Generic;
+
+namespace Repository
+{
+    public class InMemoryExcerciseRepository : ICRUD<Excercise>
+    {
+        public InMemoryExcerciseRepository()
+        {
+        }
+
+        public int Create(Excercise excercise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Excercise Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
